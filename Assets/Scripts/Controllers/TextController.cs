19e0f738da5edf3dcb9227a2cad69c35e5234dcb
Heylene <hpretzler@uni-osnabrueck.de>
2021-09-30 21:36:23 +0200

    TextController Trigger
    hat jetzt ordentliche auslöser (die man auch noch komplizierter machen könnte, so dass das spiel mal n spiel ist). Aber der Text lässt sich noch nicht ändern fml

--- Assets/Scripts/Controllers/TextController.cs
+++ Assets/Scripts/Controllers/TextController.cs
@@ -1,29 +1,55 @@
 using UnityEngine;
+using TMPro;
 
 public class TextController : MonoBehaviour {
 
     public Animator anim;
 
-    //quick fix damit es nur einmal closed, wird noch besser
-    public int temp;
+    //one trigger per textfeld animation (so they can never play at once)
+    private bool trigger1;
+    private bool trigger2;
+    private bool trigger3;
+    private bool trigger4;
 
-    // Make it the actual animator
+    public TMP_Text tmpobject;
+    
     void Start()
     {
         anim = GetComponent<Animator>();
-        temp = 0;
-        //Debug.Log("hi");   
+        tmpobject = GetComponent<TMP_Text>();
+        trigger1 = true; // first textfeld opens immediately
+        trigger2 = false;
+        trigger3 = false;
+        trigger4 = false;   
     }
 
-    // Windows are supposed to close with space key
+    
     void Update()
     {
-        
-        if (Input.GetKeyDown("space") && (temp == 0)) {
+        // close first window when space is pressed
+        if (Input.GetKeyDown("space") && (trigger1 == true)) {
 
-            temp = 1;
-            //Debug.Log("drin");
             anim.Play("close");
-        }       
+            trigger1 = false; //this should not happen again
+            trigger2 = true; //it's the next textfeld's turn
+            //tmpobject.text = "The text is different now hehe"; //change the content of the textfeld
+
+        }  
+
+        //TESTTEST hat sich der trigger wirklich geändert
+        if (Input.GetKeyDown("1") && (trigger2 == true)) {
+
+            anim.Play("pop");
+            trigger2 = false;
+            trigger3 = true;
+        }  
+
+        if (Input.GetKeyDown("2") && (trigger3 == true)) {
+
+            anim.Play("close");
+            trigger3 = false;
+            trigger4 = true;
+
+        }     
     }
 }
