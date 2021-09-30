f2644b26709f4e5dfa9d93f332967c6aee8e4ba9
Heylene <hpretzler@uni-osnabrueck.de>
2021-09-30 22:50:57 +0200

    tür anim
    ich glaub ich habs anders gemacht als du wolltest aber ich hatte spaß

--- Assets/Scripts/doorAni.cs
+++ Assets/Scripts/doorAni.cs
@@ -1,9 +1,3 @@
-//Attach this script to a GameObject with an Animator component attached.
-//For this example, create parameters in the Animator and name them “Crouch” and “Jump”
-//Apply these parameters to your transitions between states
-
-//This script allows you to trigger an Animator parameter and reset the other that could possibly still be active. Press the up and down arrow keys to do this.
-
 using UnityEngine;
 
 public class doorAni : MonoBehaviour
@@ -14,7 +8,7 @@ public class doorAni : MonoBehaviour
 
     void Start()
     {
-        //Get the Animator attached to the GameObject you are intending to animate.
+        //Getting the Animator attached to the GameObject we are intending to animate.
         m_Animator = GetComponent<Animator>();
         padlockInst = PadlockController.Instance;
 
